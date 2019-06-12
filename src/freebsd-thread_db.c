42f0a46630428d23f4d5c4c7442a886bb6b3fddc
Rajendra S Y <syrajendra@juniper.net>
2019-06-12 10:38:34 +0530

    Support fetching TLS address from -static executable

--- src/freebsd-thread_db.c
+++ src/freebsd-thread_db.c
@@ -39,9 +39,110 @@
 
 #ifdef HAVE_THREAD_DB_H
 
+#include <string.h>
 #include "global.h"
 #include "target.h"
 #include "thread_db_priv.h"
+#include <sys/ptrace.h>
+
+#if  defined(__arm__) || defined(__i386__)
+  #define LADDR unsigned int
+#else
+  #define LADDR unsigned long long
+#endif
+
+
+int read_thread_info;
+LADDR  list_addr;
+int off_tcb, off_tid, off_next, off_tlsindex, off_linkmap, off_dtv;
+
+
+unsigned long read_segment_register()
+{
+  unsigned long addr = 0;
+#if defined(__i386__)
+  int ret = ptrace(PT_GETGSBASE, CURRENT_PROCESS_PID, (caddr_t)&addr, 0);
+  if (-1 == ret) {
+    printf("Error: Failed to ptrace PT_GETGSBASE\n");
+  } else {
+    DBG_PRINT("segment gs addr: 0x%lx\n", addr);
+  }
+#elif defined (__amd64__)
+  int ret = ptrace(PT_GETFSBASE, CURRENT_PROCESS_PID, (caddr_t)&addr, 0);
+  if (-1 == ret) {
+    printf("Error: Failed to ptrace PT_GETFSBASE\n");
+  } else {
+    DBG_PRINT("segment fs addr: 0x%lx\n", addr);
+  }
+#else
+  DBG_PRINT("Error: Platform not supported\n");
+#endif
+  return addr;
+}
+
+uintptr_t read_symbol(const char *sym)
+{
+  uintptr_t sym_addr = 0;
+
+  if (symbol_lookup(sym, &sym_addr) == RET_ERR) {
+    DBG_PRINT("Error: '%s' symbol not found\n", sym);
+  } else {
+    DBG_PRINT("Found: '%s' symbol addr %lx\n", sym, sym_addr);
+  }
+  return sym_addr;
+}
+
+static psaddr_t extract_value(uint8_t *buf, size_t size)
+{
+  psaddr_t val = 0;
+  uint8_t *p;
+  uint8_t *startaddr = buf;
+  uint8_t *endaddr   = startaddr + size;
+  p = endaddr - 1;
+  for (; p>=startaddr; --p) {
+    val = (val << 8) | *p;
+  }
+  return val;
+}
+
+int read_symbol_val(const char *sym)
+{
+  uintptr_t sym_addr = read_symbol(sym);
+  uint8_t buf[64];
+  size_t read_size = 0;
+  if (_target.ph.target->read_mem(_target.ph.pid,
+                                  (uint64_t) sym_addr,
+                                  buf,
+                                  sizeof(sym_addr),
+                                  &read_size) == RET_ERR) {
+    DBG_PRINT("Error: Failed to read sym_addr %lx\n", sym_addr);
+    return RET_ERR;
+  }
+  int val = extract_value(buf, read_size);
+  return val;
+}
+
+static void read_thread_vars()
+{
+  /* freebsd repo lib/libthread_db/libpthread_db.c */
+  if (!read_thread_info) {
+    list_addr = read_symbol("_thread_list");
+    DBG_PRINT("list_addr: 0x%lx\n", (uintptr_t)list_addr);
+    off_tcb   = read_symbol_val("_thread_off_tcb");
+    DBG_PRINT("off_tcb: 0x%x\n", (unsigned int)off_tcb);
+    off_tid   = read_symbol_val("_thread_off_tid");
+    DBG_PRINT("off_tid: 0x%x\n", (unsigned int)off_tid);
+    off_next  = read_symbol_val("_thread_off_next");
+    DBG_PRINT("off_next: 0x%x\n", (unsigned int)off_next);
+    off_tlsindex  = read_symbol_val("_thread_off_tlsindex");
+    DBG_PRINT("off_tlsindex: 0x%x\n", (unsigned int)off_tlsindex);
+    off_linkmap  = read_symbol_val("_thread_off_linkmap");
+    DBG_PRINT("off_linkmap: 0x%x\n", (unsigned int)off_linkmap);
+    off_dtv  = read_symbol_val("_thread_off_dtv");
+    DBG_PRINT("off_dtv: 0x%x\n", (unsigned int)off_dtv);
+    read_thread_info = 1;
+  }
+}
 
 void thread_db_cleanup()
 {
@@ -64,9 +165,10 @@ int initialize_thread_db(pid_t pid, struct gdb_target_s *t)
       _target.ph.pid = 0;
       _target.ph.target = NULL;
       return RET_ERR;
-      
+
     case TD_OK:
       /* Thread library detected */
+      read_thread_vars();
       return RET_OK;
 
     default:
@@ -75,6 +177,72 @@ int initialize_thread_db(pid_t pid, struct gdb_target_s *t)
       _target.ph.target = NULL;
       return RET_ERR;
     }
+  return RET_OK;
+}
+
+static int get_static_tls_address(uint64_t offset, uintptr_t *tlsaddr)
+{
+  uint8_t buf[64];
+  size_t read_size;
+
+  /* freebsd repo lib/libthread_db/libthr_db.c function pt_thr_tls_get_addr() */
+  #define TARGET_READ_MEM(addr) \
+      read_size = 0; \
+      if (_target.ph.target->read_mem(_target.ph.pid, \
+                                      (uint64_t) addr, \
+                                      buf, \
+                                      sizeof(addr), \
+                                      &read_size) == RET_ERR) { \
+        DBG_PRINT("Error: Failed to read addr 0x%x\n", (unsigned int)addr); \
+        return RET_ERR; \
+      }
+
+  TARGET_READ_MEM(list_addr);
+  LADDR thr_list = extract_value(buf, read_size);
+  DBG_PRINT("thr_list: 0x%x\n", (unsigned int)thr_list);
+
+  while (thr_list != 0) {
+    LADDR raddr = thr_list + off_tid;
+  	TARGET_READ_MEM(raddr);
+	  int lwp = extract_value(buf, read_size);
+  	DBG_PRINT("LWP : %d TID: %d\n", lwp, CURRENT_PROCESS_TID);
+    if (lwp == CURRENT_PROCESS_TID)
+      break;
+    raddr = thr_list + off_next;
+    TARGET_READ_MEM(raddr);
+	  thr_list = extract_value(buf, read_size);
+  }
+  DBG_PRINT("Variable offset: 0x%lx\n", (unsigned long)offset);
+  LADDR raddr = thr_list + off_tcb;
+  TARGET_READ_MEM(raddr);
+  LADDR tcb_addr = extract_value(buf, read_size);
+  DBG_PRINT("tcb_addr: 0x%x\n", (unsigned int)tcb_addr);
+
+#if  defined(__arm__)
+  int tls_offset = 0x8;
+  *tlsaddr = (psaddr_t) tcb_addr + offset + tls_offset;
+#elif defined(__aarch64__)
+  int tls_offset = 0x10;
+  *tlsaddr = (psaddr_t) tcb_addr + offset + tls_offset;
+#elif defined(__i386__) || defined(__amd64__)
+  int    dtv_index  = 0x0;
+  unsigned long seg_base = read_segment_register();
+  DBG_PRINT("seg_base: 0x%x <equals> tcb_addr: 0x%x\n", seg_base, (unsigned int)tcb_addr);
+  raddr = tcb_addr + off_dtv;
+  TARGET_READ_MEM(raddr);
+  LADDR dtv_addr = extract_value(buf, read_size);
+  DBG_PRINT("dtv_addr: 0x%x\n", (unsigned int)dtv_addr);
+#if defined(__amd64__)
+  dtv_index = 0x10;
+#elif defined(__i386__)
+  dtv_index = 0x8;
+#endif
+  raddr = dtv_addr + dtv_index;
+  TARGET_READ_MEM(raddr);
+  LADDR tls_addr = extract_value(buf, read_size);
+  DBG_PRINT("tls_addr: 0x%x\n", (unsigned int)tls_addr);
+  *tlsaddr = (psaddr_t) tls_addr + offset;
+#endif
   return RET_OK;
 }
 
@@ -87,7 +255,9 @@ int thread_db_get_tls_address(int64_t thread, uint64_t lm, uint64_t offset,
 
   if (_target.thread_agent == NULL)
     return RET_ERR;
-  
+
+  if (lm == 0x0) return get_static_tls_address(offset, tlsaddr);
+
   err = td_ta_map_id2thr(_target.thread_agent, thread, &th);
   if (err)
     return RET_ERR;
